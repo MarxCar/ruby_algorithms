0d4126e561adb79911936bd8d34dbd74e3fc2c39
MarxCar <sam_hannat@outlook.com>
2016-03-28 20:22:40 -0400

    Omitted Debugging output to terminal and changed formatting

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,22 +2,20 @@
 #Author: Sammy Hannat
 #Description: Sorting algorithm in O(n^2)
 def bubble_sort arr
-	len = arr.count - 1
+  len = arr.count - 1
 	
-	loop do
-		swapped = false
-		len.times do |i|
-			
-			if arr[i] > arr[i+1]
-				puts "#{arr[i]} is greater than #{arr[i+1]}"
-				arr[i], arr[i+1] = arr[i+1], arr[i]
-			    
-				swapped = true
-			end
-		end
-		break unless swapped
-	end
-	return arr
+  loop do
+    swapped = false
+    len.times do |i|
+      if arr[i] > arr[i+1]
+      	arr[i], arr[i+1] = arr[i+1], arr[i]
+      	swapped = true
+      end
+    end
+  break unless swapped
+  end
+  
+  return arr
 end
 
 array = [90, 80, 99, 1001, 890]
