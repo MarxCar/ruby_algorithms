d9b3d9c5038053d86885a0820a0344ed0fbe8726
Marxcar <sam_hannat@outlook.com>
2016-05-17 22:40:51 -0400

    Fixed build_tree method

--- depth_and_breadth_first_search.rb
+++ depth_and_breadth_first_search.rb
@@ -14,6 +14,21 @@ class Node
 end
 
 
-def build_tree arr
 
+def build_tree arr,tree
+	unless arr.length <= 1
+		current = Node.new(arr.median)
+		puts "Median is #{current.val}"
+		mid = arr.length/2
+		tree << current
+		current.left = build_tree arr[0,mid], tree
+		current.right = build_tree arr[mid, arr.length], tree
+		return current.val
+	else
+		arr.delete(arr[0])
+		return arr[0]
+	end
 end
+
+build_tree [1,2,3,4,5,6,7,8, 9], tree=Array.new
+print tree
